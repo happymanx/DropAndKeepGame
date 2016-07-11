680eafbffaad152f8bad8949e05f6420c330d125
Jason <happyman.cg@gmail.com>
2016-07-11 17:53:27 +0800

    Added project files

---
+++ SpriteKitSimpleGame/GameOverScene.h
@@ -0,0 +1,15 @@
+//
+//  GameOverScene.h
+//  SpriteKitSimpleGame
+//
+//  Created by Main Account on 9/4/13.
+//  Copyright (c) 2013 Razeware LLC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+ 
+@interface GameOverScene : SKScene
+ 
+-(id)initWithSize:(CGSize)size won:(BOOL)won;
+ 
+@end
